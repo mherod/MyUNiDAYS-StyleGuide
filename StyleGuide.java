e04d4a2a42b2e6a0e77106a76f1873e08c43c585
Matthew Herod <matthew.herod@myunidays.com>
2017-10-30 11:08:34 +0000

    More progress on StyleGuide.java

--- StyleGuide.java
+++ StyleGuide.java
@@ -5,17 +5,21 @@ public class StyleGuide {
 
     // spaces over tabs
 
-    // public constants
+    // constants first, public then private
+
     public static final int FRIENDLY_NUMBER = 800;
 
-    // private constants
     private static final int MAGIC_NUMBER = 200;
 
+    // properties after constants
+    private String name;
+
+    // public static functions
     public static void main(String[] args) {
 
         // camelcase variable names
         // try to default where sensible
-        String input = getFirstArgOrDefault(args, "0");
+        String input = Utils.getFirstArgOrDefault(args, "0");
 
         int inputAsInt;
         try {
@@ -29,16 +33,50 @@ public class StyleGuide {
 
         int biggestNum = Math.max(FRIENDLY_NUMBER, inputAsInt);
 
-        int result = performSpecialCalculation(biggestNum);
+        int result = BusinessLogic.performSpecialCalculation(biggestNum);
 
         System.out.println("Result " + result);
+
+        final StyleGuide styleGuide = new StyleGuide("example");
+
+        String styleGuideName = styleGuide.getName();
+
+        System.out.println("Name " + styleGuideName);
     }
 
-    private static String getFirstArgOrDefault(String[] args, String defaultValue) {
-        return Arrays.stream(args).findFirst().orElse(defaultValue);
+    // constructor after:
+    //  - fields
+    //  - public static methods (entry points to the class)
+
+    public StyleGuide(String name) {
+        this.name = name;
     }
 
-    private static int performSpecialCalculation(int input) {
-        return input + MAGIC_NUMBER;
+    // declare methods after all field declarations
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // private static functions
+
+    // inner classes, public then private
+
+    public static class Utils {
+
+        static String getFirstArgOrDefault(String[] args, String defaultValue) {
+            return Arrays.stream(args).findFirst().orElse(defaultValue);
+        }
+    }
+
+    private static class BusinessLogic {
+
+        private static int performSpecialCalculation(int input) {
+            return input + MAGIC_NUMBER;
+        }
     }
 }
